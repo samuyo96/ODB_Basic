385b1cec7dfca0c31e3a362f017c8ab3dd3a1a4c
Samuel Noval Ruiz <76442019+samuyo96@users.noreply.github.com>
2025-04-17 13:45:45 +0200

    20251704
    Conexion mediante Node.js

---
+++ dbConnection.js
@@ -0,0 +1,63 @@
+﻿const oracledb = require('oracledb');
+
+const dbConfig = {
+	user: "SAMUEL",
+	password: "1234",
+	connectString: "localhost:1521/FREE"
+}
+
+async function connectTest() {
+	let connection;
+
+	try {
+		connection = await oracledb.getConnection(dbConfig);
+		console.log("Conectado");
+	} catch (err) {
+		console.log("error en conexión", err);
+	} finally {
+		if (connection){
+			try {
+				await connection.close();
+			}catch (err){
+				console.log(err);
+			}
+		}
+	}
+};
+
+async function connectToSamurai () {
+	try {
+		const connection = await oracledb.getConnection(dbConfig);
+		console.log("Conectado");
+		return connection;
+	} catch (err) {
+		console.error ("error en conexión", err);
+		throw err;
+	}
+}
+
+async function disconnectToSamurai(connection) {
+	try{
+		if (connection) {
+			await connection.close();
+			console.log("Desconexión")
+		
+		}
+	} catch (err) {
+		console.error ("error en desconexión", err);
+		throw err;
+	}
+}
+
+async function executeProcedure (connection, procedureName, params[]){
+	try {
+		const result = await connection.execute(
+			`BEGIN ${procedureName}(:params); END;`, 
+			{params}
+		);
+		console.log ("Resultado:", result);
+	} catch (err) {
+		console.error ("error al usar el procedimiento " + procedureName, err);
+		throw err;
+	}
+}
